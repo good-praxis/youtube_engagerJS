de391c1d9cff2cadc7bce3a8622cd0f10c621159
Praxis <hello@vulpinecat.com>
2020-10-14 15:00:03 +0200

    add eslint

--- components/Dropzone.jsx
+++ components/Dropzone.jsx
@@ -3,19 +3,18 @@ import { useDropzone } from 'react-dropzone'
 import { FileContext } from './FileContext'
 
 export default function Dropzone() {
-  const [file, setFile] = useContext(FileContext)
+  const [, setFile] = useContext(FileContext)
   const onDrop = useCallback((acceptedFiles) => {
     acceptedFiles.forEach((incoming) => {
       setFile(incoming)
     })
   }, [])
   const { getRootProps, getInputProps } = useDropzone({ onDrop })
-  console.log(file)
 
   return (
     <div {...getRootProps()}>
       <input {...getInputProps()} />
-      <p>Drag 'n' drop some files here, or click to select files</p>
+      <p>Drag &apos;n&apos; drop some files here, or click to select files</p>
     </div>
   )
 }
